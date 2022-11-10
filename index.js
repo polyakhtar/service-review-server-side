b9409658cc0013cb880266c9bcd1e952b09901ea
polyakhtar <poly09876pm@gmail.com>
2022-11-10 18:26:37 +0600

    myreviews api added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express=require('express');
 const app=express();
 require('dotenv').config();
 const cors=require('cors');
+const jwt=require('jsonwebtoken');
 const port=process.env.PORT||5000;
 
 
@@ -11,15 +12,32 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mjqzqbo.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function veriifyJWT(req,res,next){
+    const authheader=req.headers.authorization;
+    if(!authheader){
+       return res.status(401).send({message:'unauthorized access'})
+    }
+    const token=authheader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
+        if(err){
+          return  res.status(403).send({message:'unauthorized access'})
+        }
+        req.decoded=decoded;
+        next();
+    })
+}
 async function run(){
     try{
     const serviceCollection=client.db('weddingPhotographer').collection('services');
     const reviewCollection=client.db('weddingPhotographer').collection('reviews');
+    app.post('/jwt',(req,res)=>{
+        const user=req.body;
+        const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'});
+        res.send({token});
+    })
     app.get('/services',async(req,res)=>{
         const query={};
         const cursor=serviceCollection.find(query).limit(3);
@@ -55,27 +73,58 @@ async function run(){
             query={
                 email:req.query.email,
             
-            }
-           
+            }   
         }
     const cursor=reviewCollection.find(query);
     const reviews=await cursor.toArray();
     res.send(reviews)
     })
-    app.patch('/reviews/:id',async(req,res)=>{
+    app.post('/myreviews',async(req,res)=>{
+        const review=req.body;
+        const result=await  reviewCollection.insertOne(review);
+       res.send(result);
+    })
+    app.get('/myreviews',async(req,res)=>{
+    //     const decoded=req.decoded;
+    // console.log('inside orders api',decoded)
+    // if(decoded.user!==req.query._id){
+    //   return res.status(403).send({message:'unauthorized message'})
+    // }
+        let query={};
+        if(req.query.email){
+            query={
+                email:req.query.email,
+            
+            }   
+        }
+    const cursor= reviewCollection.find(query);
+    const reviews=await cursor.toArray();
+    res.send(reviews)
+    })
+    app.get('/myreviews/:id',async(req,res)=>{
         const id=req.params.id;
-        const status=req.body.status;
         const query={_id:ObjectId(id)};
-        const updateDoc={
+        const myreview=await reviewCollection.findOne(query);
+        res.send(myreview);
+    })
+    app.put('/myreviews/:id',async(req,res)=>{
+        const id =req.params.id;
+        const filter={_id:ObjectId(id)};
+        const review=req.body;
+        const option={upsert:true};
+        const updatedReview={
             $set:{
-                status:status
+                photoUrl:review.photourl,
+             customer:review.customer,
+            message:review.message,
             }
         }
-        const result=await reviewCollection.updateOne(query,updateDoc);
-        res.send(result);
-    
+        const result=await reviewCollection.updateOne(filter,updatedReview,option);
+        res.send(result)
+        
     })
-    app.delete('/reviews/:id',async(req,res)=>{
+
+    app.delete('/myreviews/:id',async(req,res)=>{
         const id=req.params.id;
         const query={_id:ObjectId(id)};
         const result=await reviewCollection.deleteOne(query);
@@ -92,5 +141,5 @@ app.get('/',(req,res)=>{
     res.send('photographer api running')
 })
 app.listen(port,()=>{
-    console.log(`photgrapher port running on port${port}`)
+    console.log(`photgrapher port running on port ${port}`)
 })
