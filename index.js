be5abb0d79f19432c0ce3c5570149ed1b645df86
polyakhtar <poly09876pm@gmail.com>
2022-11-10 19:42:04 +0600

    all api created

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ async function run(){
         res.send(result)
     })
 
+
     app.delete('/myreviews/:id',async(req,res)=>{
         const id=req.params.id;
         const query={_id:ObjectId(id)};
